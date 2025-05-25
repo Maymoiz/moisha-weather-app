22a7851f169ce6b59280206fae853aa86c8f73bb
Maymoiz <Moishamayndlovu@gmail.com>
2025-05-25 12:22:45 +0200

    Added functionality to my search engine
    selected the form and h1 in javascript

--- javascript.js
+++ javascript.js
@@ -1,2 +1,10 @@
+function searchSubmit(event){
+    event.preventDefault();
+let searchInput = document.querySelector("#search-input");
+let h1 = document.querySelector("#weather-city");
+h1.innerHTML = searchInput.value;
+
+}
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", searchSubmit);
