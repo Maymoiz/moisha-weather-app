bc6fc84d6be4b61573aee3efef46fd33eb5cd9a4
Maymoiz <Moishamayndlovu@gmail.com>
2025-05-26 15:13:48 +0200

    added correct information to the main
    -added the temperature ,date and time that will always update to the correct information for the searched city

--- javascript.js
+++ javascript.js
@@ -1,28 +1,51 @@
-function updateWeather(response){
-    let temperature = document.querySelector("#app-temp")
+function updateWeather(response) {
+    let temperature = document.querySelector("#app-temp");
+    console.log(response.data);
     temperature.innerHTML = Math.round(response.data.temperature.current);
-    let city = document.querySelector("#app-city")
-    city.innerHTML = response.data.city;
-    console.log()
-
+let city = document.querySelector("#app-city");
+    console.log(response.data);
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.condition.description;
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = `${Math.round(response.data.temperature.humidity)} %`;
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = `${Math.round(response.data.wind.speed)}km/hr`;
+    let dateElement = document.querySelector("#time");
+    let dateInMilliseconds = response.data.time * 1000;
+    dateElement.innerHTML = formatDate(dateInMilliseconds);
+    
+}
+function formatDate(date) {
+    date = new Date(date);
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+return` ${day} ${hours}:${minutes}`;
 }
 
-function searchCity(city){
-    apiKey = "6b0de8c4f230fd2bf4t68daf5046oe9a";
-    apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit="metric"`;
-    axios.get(apiUrl).then(updateWeather)
+function searchCity(city) {
+    const apiKey = "6b0de8c4f230fd2bf4t68daf5046oe9a";
+    const apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+    console.log(apiUrl);
+
+    axios.get(apiUrl)
+        .then(updateWeather)
+        .catch(error => console.error('Error fetching weather data:', error));
 }
+    
 
-
-function searchSubmit(event){
+function searchSubmit(event) {
     event.preventDefault();
-let searchInput = document.querySelector("#search-input");
-let h1 = document.querySelector("#weather-city");
-h1.innerHTML = searchInput.value;
-searchCity(searchInput.value);
-
+    let searchInput = document.querySelector("#search-input");
+    let h1 = document.querySelector("#weather-city");
+    h1.innerHTML = searchInput.value; 
+    searchCity(searchInput.value); 
 }
-searchCity("Pretoria");
 
+searchCity("Pretoria");
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchSubmit);
+searchForm.addEventListener("submit", searchSubmit); 
